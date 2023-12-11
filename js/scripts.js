c6d5a07c55707d6236fb242133dccb148c8f8cfc
Chels93 <148895873+Chels93@users.noreply.github.com>
2023-12-11 10:58:31 -0500

    variable pokemonList created
    three objects added to array: Eevee, Blastoise, Charizard. Name, height and type were three keys added to each object.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
-alert('Hello world');
-let favoriteFood = 'Mangoes';
-document.write(favoriteFood);
+let pokemonList = [];
+
+pokemonList.push({
+    name: "Eevee",
+    height: 0.3,
+    type: ["normal"]
+});
+
+pokemonList.push({
+    name: "Blastoise",
+    height: 1.6, 
+    type: ["water"]
+});
+
+pokemonList.push({
+    name: "Charizard",
+    height: 1.7, 
+    type: ["fire", "flying"]
+});
