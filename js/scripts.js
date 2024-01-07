d7f0f538d95a906ffac0369c7a71d1a5b1419e7b
Chels93 <148895873+Chels93@users.noreply.github.com>
2024-01-07 15:07:06 -0500

    Corrected functions, forEach()

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,43 @@
-// Pokemon height is in meters
-
-// let pokemonList = [];
-
-// pokemonList.push({
-//     name: "Eevee",
-//     height: 0.3,
-//     type: ["normal"]
-// });
-
-// pokemonList.push({
-//     name: "Blastoise",
-//     height: 1.6, 
-//     type: ["water"]
-// });
-
-// pokemonList.push({
-//     name: "Charizard",
-//     height: 1.7, 
-//     type: ["fire", "flying"]
-// });
-
-//     for (let i = 0; i < pokemonList.length; i++) {
-//         document.write("<p class='pokemon-container'>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")</p>")
-
-//        If Pokemon is above 1 meter, add a special label 
-//              if (pokemonList[i].height >1.0) 
-//              document.write("<div class='big-label'> - Wow, that's big!</div>");
-// }
-
-// forEach() function to iterate through pokemonList
-pokemonList.forEach(function (pokemon) {
-    console.log(pokemonList.name + 'is' + pokemonList.height + 'meters tall.');
-}
-                    
-// pokemonList array wrapped in IIFE
-// new variable to hold what IIFE will return 
+//pokemon height is in meters                
 let pokemonRepository = (function () {
-        let pokemonList = [];
+    
+  let pokemonList = [
+    {
+        name: "Eevee",
+        height: 0.3,
+        type: ["normal"]
+    },
+    {
+        name: "Blastoise",
+        height: 1.6,
+        type: ["water"]
+    },
+    {
+        name: "Charizard",
+        height: 1.7,
+        type: ["fire", "flying"]
+    }
+  ]
 
-         pokemonList.push({
-            name: "Eevee",
-            height: 0.3,
-            type: ["normal"]
-        });
+    function getAll () {
+      return pokemonList;
+    }
+    function add (pokemon) {
+        pokemonList.push(pokemon);
+    }
+  
+    return {
+        getAll: getAll,
+        add: add
+    }
+      
+})()
 
-        pokemonList.push({
-            name: "Blastoise",
-            height: 1.6,
-            type: ["water"]
-        });
+pokemonRepository.getAll().forEach(function (pokemon) {
+  console.log(pokemon.name + 'is' + pokemon.height + 'meters tall.');
+});
+  
 
-        pokemonList.push({
-            name: "Charizard",
-            height: 1.7,
-            type: ["fire", "flying"]
-        });
-        console.log(pokemon);
-
-        return {
-            add: function (pokemon) {
-                pokemonList.push(pokemon);
-            },
-            getAll: function () {
-                return pokemonList;
-            }
-        };
-
-        // updated loop to reflect public returns   
-        pokemonRepository.getAll().forEach(function (pokemon) {
-            pokemonRepository.addListItem(pokemon);
-        });
-
-    })();
+   
 
     
